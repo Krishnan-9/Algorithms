ae56d363e427a607d396bb0811898dbe27b99562
Krishnan <krishnan_pgk@yahoo.com>
2021-02-13 22:58:23 +0530

    Create Algo.py

---
+++ Algo.py
@@ -0,0 +1,27 @@
+# TYPES OF SORTING ALGORITHMS
+
+# 1. Insertion sort
+def create_array(*array, random=False):
+    # create interface
+    string = array[0]
+    ls = string.split()
+    ls = list(map(int, ls))
+    print(ls)
+
+    n=len(ls)
+    for i in range(1,n):
+        j = i-1
+        var = ls[i]
+        while j>=0 and ls[j]>var:
+            ls[j+1] = ls[j]
+            ls[j] = var
+            j-=1
+
+        else:
+            pass
+
+        print(ls)
+
+
+inp = input("enter")
+create_array(inp)
