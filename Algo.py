c5afc47d9bcf81522f47fabb3e44231db1f8e06a
Krishnan <krishnan_pgk@yahoo.com>
2021-02-14 21:17:00 +0530

    Update Algo.py

--- Algo.py
+++ Algo.py
@@ -16,7 +16,6 @@ def create_array(*array, random=False):
             ls[j+1] = ls[j]
             ls[j] = var
             j-=1
-
         else:
             pass
 
@@ -25,3 +24,28 @@ def create_array(*array, random=False):
 
 inp = input("enter")
 create_array(inp)
+
+# 2. Selection sort
+def get_min_index(ls):
+    min = ls[0]
+    for j in range(1, len(ls)):
+        if j < len(ls) and ls[j] < min:
+            min = ls[j]
+            print(min)
+    return ls.index(min)
+
+
+
+def selection_sort(ls):
+    j=0
+    while j<len(ls):
+        min_index = get_min_index(ls[j:]) + j
+        if ls[j]>ls[min_index]:
+            ls[j], ls[min_index] = ls[min_index], ls[j]
+            j+=1
+        else:
+            j+=1
+
+    return ls
+A = [8,4,6,6,7,2,2,34,5,6,7,8,8]
+print(selection_sort(A))
